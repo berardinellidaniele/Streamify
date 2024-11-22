f188b747f7e17d02f5232bd0ef5f092fe7e2920b
Daniele Berardinelli <berardinellidaniele@protonmail.com>
2024-11-22 19:20:24 +0100

    Aggiunto occhio controllo password in Login e Registrati

--- Streamify/wwwroot/js/site.js
+++ Streamify/wwwroot/js/site.js
@@ -1,4 +1,21 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener('DOMContentLoaded', function () {
+    const inputpasswordicone = document.querySelectorAll('.toggle-password');
 
-// Write your JavaScript code.
+    inputpasswordicone.forEach(function (icon) {
+        icon.addEventListener('click', function () {
+            if (this.classList.contains('disabled')) return;
+
+            const input = document.querySelector(`#${this.dataset.toggle}`);
+            const tipo = input.getAttribute('type') === 'password' ? 'text' : 'password';
+            input.setAttribute('type', tipo);
+            this.classList.toggle('fa-eye');
+            this.classList.toggle('fa-eye-slash');
+            this.classList.toggle('active');
+
+            this.classList.add('disabled');
+            setTimeout(() => {
+                this.classList.remove('disabled');
+            }, 500);
+        });
+    });
+});
